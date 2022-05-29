9328aa58898337f04265da3255fb7653f98cc2c4
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-05-29 19:54:42 +0530

    created other basic route put delete and post route

--- express/basic-routing/route.js
+++ express/basic-routing/route.js
@@ -5,12 +5,27 @@ const app = express();
 
 // r1 => GET route.
 app.get("/", (req,resp) => {
-    resp.send("you are quarying for GET route on '/'")
-})
+    resp.send("you are quarying for GET route at '/'");
+});
+
+// r2 => POST route.
+app.post("/", (req, resp) => {
+    resp.send("just got a POST request at '/' route.");
+});
+
+// r3 => PUT route.
+app.put("/", (req, resp) => {
+    resp.send("just got a PUT request at '/' route.");
+});
+
+// r4 => DELETE route.
+app.delete("/", (req, resp) => {
+    resp.send("just got a DELETE request at '/' route.");
+});
 
 app.listen(port, (err) => {
     if(err) throw err;
     else{
         console.log("app is running..");
     }
-})
+});
