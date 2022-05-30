ea993ff352cee1ca22fc6ca3e021c45863c48114
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-05-30 11:46:42 +0530

    learn about how to create multiple route and how to send response in html format as well as how to send static file

--- express/my-express-server/server.js
+++ express/my-express-server/server.js
@@ -6,8 +6,25 @@ const app = express(); // initialize express app.
 // create basic get route on /
 app.get("/", (req, resp) => {
     resp.send("app is running perfectly fine");
+});
+
+app.get("/home", (req, resp) => {
+    resp.send("<h1>Requesting at home route. </h1>");
+    // resp.send("<h2>Requesting at home route.</h2>") // error we can send only once.
+});
+
+app.get("/about", (req, resp) => {
+    resp.send("Requesting at about route");
+});
+
+app.get("/contact", (req, resp) => {
+    resp.send("requesting at contact route.")
 })
 
+
+// sending static file through resp.sendfile method.
+app.use(express.static('static'));  // now we can access all the file by using browser which is inside static file.
+// static file include html, css, js and image files.
 
 // creating app to listen at a particular port.
 app.listen(port, () => {
