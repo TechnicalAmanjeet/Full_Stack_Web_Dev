71416836dfba0468dc771fc84360258d9c9fd71e
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-05-30 14:48:06 +0530

    done with how to get data from html form and how we can parse it and send back a response.

--- nodeProject/Calculator/app.js
+++ nodeProject/Calculator/app.js
@@ -5,8 +5,20 @@ const port = 3000;
 
 
 app.get("/", (req, resp) => {
-    resp.send("Requesting at home route.");
+    const filePath = __dirname + "/index.html";
+    resp.sendFile(filePath);
 });
+
+// app.use(express.json()); // for parsing json data.
+app.use(express.urlencoded({extended:true})) // for parsing url data which comes from html forms.
+
+app.post("/", (req, resp)=> {
+    // console.log("something coming from server..")
+    const num1 = parseInt(req.body.fstNumber);
+    const num2 = parseInt(req.body.sndNumber);
+    resp.send(`sum of ${num1} and ${num2} is ${num1+num2}`);
+    // resp.send("your response sumbmitted successfully.")
+})
 
 app.listen(port, (err)=>{
     if(err) throw err;
